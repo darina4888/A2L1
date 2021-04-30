f04a690c5e7a4210c021f9d162da7824a4b11301
darina4888 <daria_fedoseeva@mail.ru>
2021-04-30 13:22:52 +0300

    A2L1

---
+++ app/src/main/java/com/example/a2l1/ObjectClass.kt
@@ -0,0 +1,9 @@
+package com.example.a2l1
+
+//позволяет объявить класс и сразу же создать его экземпляр
+object ObjectClass {
+
+    var repository: Double = 3.0
+
+    fun getOurRepository() = repository
+}
